b8813598d055623f22624a4edc1d9a0233191fe5
Garet Hough <garetshough@gmail.com>
2021-11-30 14:59:44 -0800

    chore(listContainer) added list container to project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import {useState} from 'react';
+import AddItem from './components/AddItem';
 
 function App() {
   const [listContainer,setListContainer] = useState([]);
+
+  console.log(listContainer)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        Decidr
       </header>
+      <AddItem
+        listContainer={listContainer} 
+        setListContainer = {setListContainer}
+      />
     </div>
   );
 }
---
+++ src/components/AddItem.js
@@ -0,0 +1,26 @@
+import {useState} from 'react';
+
+export default function AddItem({listContainer,setListContainer}){
+    const [newItem, setNewItem] = useState('');
+
+const formSubmit = (event) => {
+    event.preventDefault();
+    setListContainer([...listContainer,newItem]);
+    setNewItem('');
+;
+}
+
+    return(
+        <form onSubmit={formSubmit}>
+            <input 
+                id="add-item" 
+                value={newItem} 
+                onChange={event => setNewItem(event.target.value)}
+                placeholder="Add a new item"/>
+            <button 
+            type="submit"
+            onClick={formSubmit}
+            >Add Item</button>
+        </form>
+    )
+}
